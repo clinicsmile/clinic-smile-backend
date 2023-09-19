65bf8a13b1fcc684019271e433c018260f464f47
AndresMaya13 <andresmaya3113@gmail.com>
2023-09-19 01:52:18 -0500

    getBrand fix

--- src/routes/clinics.routes.js
+++ src/routes/clinics.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require('../Controller/clinic.controller');
 
-router.post("/getBrand", controller.Auth);
+router.post("/getBrand", controller.getBrand);
 
 module.exports = router;
