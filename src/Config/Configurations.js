9ba214f2dd258b70ad332f3d5967acd65cfd2f8d
Daniel Otalora <Daniel.otalora@pcpplasticos.com>
2023-10-30 12:36:30 -0500

    try fix enviroment variables

--- src/Config/Configurations.js
+++ src/Config/Configurations.js
@@ -1,12 +1,12 @@
 const configuration = {};
 
-configuration.SERVER_IP = process.SERVER_IP || "192.168.1.102";
-configuration.SERVER_PORT = process.SERVER_PORT || "5000";
+configuration.SERVER_IP = process.env.SERVER_IP || "localhost";
+configuration.SERVER_PORT = process.env.SERVER_PORT || "5000";
 
-configuration.MYSQL_HOST = process.MYSQL_HOST || "localhost";
-configuration.MYSQL_PORT = process.MYSQL_PORT || "3306";
-configuration.MYSQL_USER = process.MYSQL_USER || "root";
-configuration.MYSQL_PASSWORD = process.MYSQL_PASSWORD || "1234";
-configuration.MYSQL_DATABASE = process.MYSQL_DATABASE || "clinicsmile";
+configuration.MYSQL_HOST = process.env.MYSQL_HOST || "localhost";
+configuration.MYSQL_PORT = process.env.MYSQL_PORT || "3306";
+configuration.MYSQL_USER = process.env.MYSQL_USER || "root";
+configuration.MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || "1234";
+configuration.MYSQL_DATABASE = process.env.MYSQL_DATABASE || "clinicsmile";
 
 module.exports = configuration;
