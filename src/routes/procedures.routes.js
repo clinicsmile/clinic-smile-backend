86296104b2eb9185839f942f0b1b9f385c53a99a
Daniel Otalora <Daniel.otalora@pcpplasticos.com>
2023-10-30 23:26:05 -0500

    test register procedure

--- src/routes/procedures.routes.js
+++ src/routes/procedures.routes.js
@@ -3,8 +3,16 @@ const router = express.Router();
 const controller = require("../Controller/procedures.controller");
 const middleware = require("../Middlewares/Auth");
 
-router.post("/registerProcedure",middleware.isAuthenticated,controller.createProcedure);
-router.put("/inactivateProcedure", middleware.isAuthenticated,controller.inactivateProcedure);
+router.post("/registerProcedure", controller.createProcedure);
+router.put(
+  "/inactivateProcedure",
+  middleware.isAuthenticated,
+  controller.inactivateProcedure
+);
 
-router.get("/toListAllRegister/:document",middleware.isAuthenticated, controller.toListAllRegister);
+router.get(
+  "/toListAllRegister/:document",
+  middleware.isAuthenticated,
+  controller.toListAllRegister
+);
 module.exports = router;
